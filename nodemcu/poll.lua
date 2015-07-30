1f98a9bff815fb90e996572ce7ddd3ca1773c6e2
Marcus <best.nose@gmail.com>
2015-07-30 16:04:46 +1200

    keeping switch state sane

--- nodemcu/poll.lua
+++ nodemcu/poll.lua
@@ -1,11 +1,20 @@
---mqtt.lua  
+--poll.lua  
+--set pin mode for state gpio5 as interrupt
+gpio.mode(1,INT)
 
-print("prelaunch")
-print(node.heap())
+--change state of the switch in case of manual operation
+gpio.trig(1,"both",function(state)
+  if state == 1 then
+	print("Manual turn on for coffee")
+        m:publish("home/coffee/state","1",0,0, function(conn)
+	end )	
+  elseif state == 0 then
+        print("Manual turn off for coffee")
+        m:publish("home/coffee/state","0",0,0, function(conn)
+        end )	
+  end
+
 m = mqtt.Client("ESP8266 daikin", 180, "", "") --Last 2 values are user and password for broker
-print("client variable in mem")
-print(node.heap())
-
 
  m:lwt("lwt", "coffee", 0, 0)  
  m:on("offline", function(con)   
