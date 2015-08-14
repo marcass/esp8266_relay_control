a2434ef8ba6433a013f12e85ac45e235968dbfdb
Marcus <best.nose@gmail.com>
2015-08-14 12:44:11 +1200

    cleaned up to make connection fault tolerant

--- nodemcu/init.lua
+++ nodemcu/init.lua
@@ -1,11 +1,23 @@
--- init.lua script with timers that wait for wifi connection before executing 
--- heavy lifing
+-- init.lua 
+
+-- set the following variables in-file (short on memory)
+ssid	= ""
+pass	= ""
+id	= "coffee"		--client id for mqtt
+broker	= "192.168.0.3"	--url/ip of broker
+buser	= ""		--broker username
+bpass	= ""		--broker password
+lwtt	= "lwt/coffee"		--last will and testament topic
+ctop	= "coffee/com"		--coffee com topic
+csta	= "coffee/state"		--coffee state topic
+pinon	= 6
+pinoff 	= 5 
 
 -- Set pin state so it dones't turn heatpump on at power failure
-gpio.mode(6, gpio.OUTPUT)
-gpio.write(6, gpio.LOW)
-gpio.mode(5, gpio.OUTPUT)
-gpio.write(5, gpio.LOW)
+gpio.mode(pinon, gpio.OUTPUT)
+gpio.write(pinon, gpio.LOW)
+gpio.mode(pinoff, gpio.OUTPUT)
+gpio.write(pinoff, gpio.LOW)
 
 -- set counter variables
 local  a  = 0      -- Counter of trys to connect to wifi
@@ -13,19 +25,19 @@ local  b  = 200    -- Maximum number of WIFI Testings while waiting for connecti
 
 
 function checkWIFI() 
-  if ( a > b ) then
-    print("Sorry. Not able to connect")
-  else
-    if ( ( wifi.sta.getip() ~= nil ) and  ( wifi.sta.getip() ~= "0.0.0.0" ) )then
-      tmr.alarm( 1 , 5000 , 0 , function()
-	  --launch file
-	  dofile("poll.lua")
-      end )
-    else
-      -- Reset alarm again
-      tmr.alarm( 0 , 2500 , 0 , checkWIFI)
-      print("Checking WIFI..." .. a)
-      a = a + 1
+ if ( a > b ) then
+   print("Sorry. Not able to connect")
+ else
+   if ( ( wifi.sta.getip() ~= nil ) and  ( wifi.sta.getip() ~= "0.0.0.0" ) )then
+     tmr.alarm( 1 , 2000 , 0 , function()
+       --launch file
+       dofile("poll.lua")
+     end )
+   else
+     -- Reset alarm again
+     tmr.alarm( 0 , 2500 , 0 , checkWIFI)
+     print("Checking WIFI..." .. a)
+     a = a + 1
     end 
   end 
 end
@@ -37,8 +49,7 @@ if ( ( wifi.sta.getip() == nil ) or  ( wifi.sta.getip() == "0.0.0.0" ) ) then
   -- We aren't connected, so let's connect
   print("Configuring WIFI....")
   wifi.setmode( wifi.STATION )
---put wifi details in here
-  wifi.sta.config( "ssid", "password")
+  wifi.sta.config(ssid, password)
   print("Waiting for connection")
   tmr.alarm( 0 , 2500 , 0 , checkWIFI )  -- Call checkWIFI 2.5S in the future.
 else
@@ -46,11 +57,7 @@ else
   print("IP Address: " .. wifi.sta.getip())
   a = nil
   b = nil
-
--- allow 5s for garbage to be collected
-  tmr.alarm(0, 5000, 0, function()
-	dofile("poll.lua")
-  end)
+  dofile("poll.lua")
 end
 
 
--- nodemcu/poll.lua
+++ nodemcu/poll.lua
@@ -1,44 +1,49 @@
 --poll.lua  
+
+
 --set pin mode for state gpio5 as interrupt
 gpio.mode(1,INT)
 
 --change state of the switch in case of manual operation
 gpio.trig(1,"both",function(state)
   if state == 1 then
-	print("Manual turn on for coffee")
-        m:publish("home/coffee/state","1",2,1, function(conn)
-	end )	
+    print("Manual turn on for coffee")
+    m:publish(csta,"ON",2,1, function(conn)
+    end )	
   elseif state == 0 then
         print("Manual turn off for coffee")
-        m:publish("home/coffee/state","0",2,1, function(conn)
+        m:publish(csta,"OFF",2,1, function(conn)
         end )	
   end
 
-m = mqtt.Client("ESP8266 daikin", 180, "", "") --Last 2 values are user and password for broker
+m = mqtt.Client(id, 180, buser, bpass) --Last 2 values are user and password for broker
 
- m:lwt("coffee", "offline", 0, 0)  
- m:on("offline", function(con)   
-   print("MQTT reconnecting")
-   --do the subscription business
-   tmr.alarm(0, 1000, 1, function()
-   dofile("sub.lua")
-   end)
- end)  
+m:lwt(lwttop, "offline", 0, 0)  
+m:on("offline", function(conn)   
+  print("MQTT reconnecting")
+  dofile("offline.lua")
+end)  
 
- -- on publish message receive event  
- m:on("message", function(conn, topic, data)  
-  if (data == "ON") then
-	h = 1
-  elseif (data == "OFF") then
-	h = 0
-  end
-  dofile("switch.lua")	
-  print(node.heap())
- end)  
+-- on publish message receive event  
+m:on("message", function(conn, topic, data)  
+ if (data == "ON") then
+   h = 1
+ elseif (data == "OFF") then
+   h = 0
+ end
+ dofile("switch.lua")	
+ print(node.heap())
+end)  
 
 --do the subscription business
- tmr.alarm(0, 1000, 1, function()  
-   dofile("sub.lua")
- end)
+tmr.alarm(0, 1000, 1, function()  
+ if wifi.sta.status() == 5 and wifi.sta.getip() ~= nil then
+   tmr.stop(0)
+   m:connect(broker, 1883, 0, function(conn)
+     m:subscribe(ctop,2, function(conn)
+     end)
+   end)
+ end
+end)
 
 
