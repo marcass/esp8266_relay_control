4980525afc4b9716e66199a6b3bd7b3270a13004
Marcus <best.nose@gmail.com>
2015-08-16 05:33:14 +1200

    added stuff for debouncing control

--- nodemcu/poll.lua
+++ nodemcu/poll.lua
@@ -1,45 +1,85 @@
 --poll.lua  
 
+--To avoid debouncing:
+--Hardware:
+--resistor in series with load to button and
+-- a capacator of 10-100MuF in parallel to load (-ve pin to gnd). 
+-- T=RC where T=time constant of low pass filter
+--Software:
+--20-50ms delay before moving on through read part of callback function
 
---set pin mode for state gpio5 as interrupt
-gpio.mode(1,INT)
+--place a 10k ressitor in series with relay data line to reduce current
+
+--Variables------------------------------------------------
+--set pin mode for state pin (gpio5) as interrupt
+spin = 1
 
 --set pin mode for pushbutton toggle defaults to float on startup
--- connect with a pull-up resistor
-gpio.mode(2,INT,gpio.PULLUP)
+-- set for internal pull-up
+pin = 2
+---------------------------------------------------------
 
+--function definitions------------------------------------
+--callback fucntion for button press
 function toggle()
-  s = gpio.read(1)
-  if s = 1 then
-    gpio.write(1, gpio.LOW)
-  elseif s = 0 then
-    gpio.write(1, gpio.HIGH)    
+  s = gpio.read(pin)
+  if s = 1 then  --must be on
+    gpio.write(spin, gpio.LOW)
+    --delay to avoid debounce
+    tmr.delay(20000)
+    off()
+  --in case of reboot where state pin will be floating need nil test
+  elseif s = nil or 0  then --must be off
+    gpio.write(spin, gpio.HIGH)
+    --delay to avoid debounce
+    tmr.delay(20000)
+    on()
   else
-    print("Invalid")
+    print("Invalid: check your code")
   end
 end
 
+function on()
+  gpio.write(pinon,gpio.HIGH)
+  print("Turning coffee on..")
+  m:publish(state,"ON",2,1, function(conn)
+  end )
+end
 
---button press calls toggle function   
-gpio.trig(2,"high", toggle)
+function off()
+  gpio.write(pinon,gpio.LOW)
+  print("Turning coffee off..")
+  m:publish(state,"OFF",2,1, function(conn)
+  end)
+end)
 
---change state of the switch in case of manual operation
-
+--callback fucntion to change openhab state of the switch in case of manual operation
 function state(level)
   if level == 1 then
     print("Manual turn on for coffee")
-    m:publish(csta,"ON",2,1, function(conn)
+    m:publish(csta,"ON",2,0, function(conn)
     end )
   elseif level == 0 then
         print("Manual turn off for coffee")
-        m:publish(csta,"OFF",2,1, function(conn)
+        m:publish(csta,"OFF",2,0, function(conn)
         end )
   end
 end
+---------------------------------------------------------------------
+
+--the guts (do this if that)-------------------------------------
+--set pin mode
+gpio.mode(spin,gpio.INT)
+gpio.mode(pin,gpio.INT,gpio.PULLUP)
+
+--button press calls toggle function   
+gpio.trig(pin,"high", toggle)
   
 --update state on button press
-gpio.trig(1,"both",state)
+gpio.trig(spin,"both",state)
+----------------------------------------------------------------
 
+--mqtt for openhab------------------------------------------
 m = mqtt.Client(id, 180, buser, bpass)
 
 m:lwt(lwttop, "offline", 0, 0)  
@@ -48,16 +88,25 @@ m:on("offline", function(conn)
   dofile("offline.lua")
 end)  
 
--- on publish message receive event  
-m:on("message", function(conn, topic, data)  
+-- on published message received for coffee command event  
+m:on("message", function(conn, ctop, data)  
  if (data == "ON") then
-   h = 1
+   on()
  elseif (data == "OFF") then
-   h = 0
+   off()
  end
- dofile("switch.lua")	
  print(node.heap())
 end)  
+
+-- on published message received for coffee state event
+m:on("message", function(conn, csta, data)
+ if (data == "ON") then
+   s = 1
+ elseif (data == "OFF") then
+   s = 0
+ else print("State is garbage")
+ end
+end)
 
 --do the subscription business
 tmr.alarm(0, 1000, 1, function()  
@@ -69,5 +118,5 @@ tmr.alarm(0, 1000, 1, function()
    end)
  end
 end)
-
+---------------------------------------------------------
 
